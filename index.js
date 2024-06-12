a2a53887c3128c6d94e1c68b8144112ee67c54c6
Oscar López <119974573+xavi479@users.noreply.github.com>
2024-06-11 20:50:19 -0500

    project.bat

---
+++ index.js
@@ -0,0 +1,13 @@
+//importaciones
+
+//conexxion a la bd
+
+//configuaraciones cors: peticiones que las peticiones se hagan correctos
+
+//conversión de datos (body a objetos JS)
+
+//Configuración rutas
+
+//configuaración el servidor para escuchar las peticiones
+
+//
---
+++ start-project.bat
@@ -0,0 +1,3 @@
+start cmd.exe /k "cd /d C:\Program Files\MongoDB\Server\7.0\bin && mongod.exe --dbpath C:\data
+
+start cmd.exe /k "cd /d C:\Users\xavil\proyecto nuevo\backend_redsocial && npm run start:watch:"
