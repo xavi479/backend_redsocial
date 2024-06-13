b32de41745bc055250e701c2566b66dcd1942998
Oscar López <119974573+xavi479@users.noreply.github.com>
2024-06-12 21:09:51 -0500

    Mejora de la conxion de la terminal y la creación da la bases de datos basica...

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
-/*//Importacion
-const connection = require("./database/connection");
-
-//mensaje de bienvenida
-console.log("API NODE arriba");
-
-//conexion a la DB
-
-connection ()*/
-
-
-
 // Importaciones
 const connection = require('./database/connection');
 const express = require("express");
